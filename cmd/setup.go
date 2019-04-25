846e4f43dca54621d4ea3910462ca51c9159c788
DeGuia-Cranmer <toul.deguia-cranmer@hp.com>
2019-04-25 14:56:24 -0500

    major: Pipeline should now flow..

--- cmd/setup.go
+++ cmd/setup.go
@@ -31,7 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("You have called the setup function and the dirs have been made")
+		fmt.Println("The setup function has been called and the dirs have been made")
 	},
 }
 
